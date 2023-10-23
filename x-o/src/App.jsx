b5bb159dde64de7d2d47dc84c3e2db7f549a9bb7
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-10-23 17:37:17 +0300

    finsh lojic

--- x-o/src/App.jsx
+++ x-o/src/App.jsx
@@ -1,24 +1,66 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Boxs from "./components/Boxs"
 
 
 function App() {
-  const [boxs ,setBox]=useState(["","","","","","","","",""])
+
+
+
+  const [boxs ,setBoxs]=useState(["","","","","","","","",""])
+  // start opration
+  const [start,setStart]=useState("circle")
+
+  // console.log(boxs)
+  const [win, setWin]=useState("")
+  const Oddsofwinning =
+  [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+  ]
+ useEffect(()=>{
+ 
+   Oddsofwinning.forEach((win)=>{
+
+   const circleWin = win.every((box)=>boxs[box]=== "circle")
+   const crosswin = win.every((box)=>boxs[box]=== "cross")
+    if(circleWin){
+    setWin("circle Win")
+    }else if(crosswin){
+     setWin("cross Win")
+ 
+    }
+   })
+ },[boxs,win])
+
+useEffect(()=>{
+ if (boxs.every((box)=>box !== "")&&!win) {
+  setWin("Draw")
+ }
+
+},[boxs,win])
   return (
     <>
    <div className="section" >
-    <h1>my game tic tac to</h1>
+   
    <div className="container">
           <div className="content">
 
             {boxs.map((box,index)=>(
 
-              <Boxs key={index} />
+              <Boxs id={index}win={win} key={index} start={start} box={box} setStart={setStart} boxs={boxs} setBoxs={setBoxs}/>
 
             )) 
             }
           </div>
      </div>
+     <h3>{win}</h3>
+     {!win &&<div>{` its now ${start} turn`}</div>}
      <button style={{marginTop:'30px',background:'#111'}}>Reaset</button>
    </div>
     </>
--- x-o/src/components/Boxs.jsx
+++ x-o/src/components/Boxs.jsx
@@ -1,7 +1,46 @@
-const Boxs = () => {
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-unused-vars */
+
+
+/* eslint-disable react/prop-types */
+const Boxs = ({start ,win,setStart , id , box,boxs,setBoxs}) => {
+
+
+
+
+
+
+  const handleClick = (e) =>{
+    if (win) {
+      
+      return;
+    }
+       const nocilcked =!boxs[id]
+     if (nocilcked) {
+        if(start ==="circle"){
+          handleChangeMark("circle")
+          setStart("cross")
+        }
+
+        if(start === "cross"){
+          handleChangeMark("cross")
+          setStart("circle")
+        }
+     }
+
+       console.log(id)
+  }
+  //  handle change 
+  const handleChangeMark =(boxschange)=>{
+    let copyboxs= [...boxs] // coby array
+    copyboxs[id]=boxschange //new valu (circle && close)
+    setBoxs(copyboxs) 
+  }
   return (
     <>
-    <div className="sqware">O</div>
+    <div className="sqware" onClick={handleClick}>
+      <div className={box } >{box ?(box ==="circle" ? 'o': "x"):""}</div>
+    </div>
     </>
   )
 }
